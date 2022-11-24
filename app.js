689bf534f483b0eec8fc89ba1943066b947ad88b
emmateva18 <emmateva18@codingburgas.bg>
2022-11-24 10:25:42 +0200

    Finish

---
+++ app.js
@@ -0,0 +1,6 @@
+const accorian = document.querySelectorAll('.content-container');
+for(i = 0; i < accorian.length; i++) {
+    accorian[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+}
